ad8e57f6da14b4d084bfa61653b7fc85cf3afe09
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2024-04-07 14:51:13 +0200

    feat: Automatically extract services.json from v3 map API

---
+++ Generators/extract_services.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+TARGET_DIR=$(cd $(dirname "$0")/.. >/dev/null 2>&1 && pwd)/Reference
+
+cd $TARGET_DIR
+
+TARGET="services.json"
+
+# Download the json file from Wynncraft API
+wget -O markers.json.tmp "https://api.wynncraft.com/v3/map/locations/markers"
+
+if [ ! -s markers.json.tmp ]; then
+    rm markers.json.tmp
+    echo "Error: Wynncraft API is not working, aborting"
+    exit
+fi
+
+# Input JSON file name
+inputFile="markers.json.tmp"
+
+missingFile="../Data-Storage/services_missing.json"
+
+# Transform the input using jq directly
+jq '
+  # Define the mappings as a key-value object
+  def mappings: {
+    "Merchant_Armour.png": "Armour Merchant",
+    "Merchant_Dungeon.png": "Dungeon Scroll Merchant",
+    "Merchant_Emerald.png": "Emerald Merchant",
+    "Merchant_Liquid.png": "Liquid Merchant",
+    "Merchant_Potion.png": "Potion Merchant",
+    "Merchant_Scroll.png": "Scroll Merchant",
+    "Merchant_Tool.png": "Tool Merchant",
+    "Merchant_Weapon.png": "Weapon Merchant",
+    "NPC_Blacksmith.png": "Blacksmith",
+    "NPC_ItemIdentifier.png": "Item Identifier",
+    "NPC_PowderMaster.png": "Powder Master",
+    "NPC_TradeMarket.png": "Trade Market",
+    "Profession_Alchemism.png": "Alchemism Station",
+    "Profession_Armouring.png": "Armouring Station",
+    "Profession_Cooking.png": "Cooking Station",
+    "Profession_Jeweling.png": "Jeweling Station",
+    "Profession_Scribing.png": "Scribing Station",
+    "Profession_Tailoring.png": "Tailoring Station",
+    "Profession_Weaponsmithing.png": "Weaponsmithing Station",
+    "Profession_Woodworking.png": "Woodworking Station",
+    "Special_FastTravel.png": "Fast Travel",
+    "Special_HousingAirBalloon.png": "Housing Balloon",
+    "Special_SeaskipperFastTravel.png": "Seaskipper"
+  };
+
+  # Transform the array of items
+  map(
+    {
+      type: mappings[.icon],
+      locations: [{ x: (.x | tonumber), y: (.y | tonumber), z: (.z | tonumber) }]
+    }
+  )
+  # Filter out any items that could not be mapped
+  | map(select(.type != null))
+  # Group by type and merge locations
+  | group_by(.type)
+  | map({
+      type: .[0].type,
+      locations: map(.locations[]) | unique_by({x, y, z})
+    })
+' "$inputFile" > services.json.tmp
+
+# Combine the files and handle processing
+jq -s '
+  # Combine arrays from both files
+  add |
+  # Group by type to merge types present in both files
+  group_by(.type) |
+  # Map each group to a single object consolidating the locations
+  map({
+    type: .[0].type,
+    locations: (map(.locations[]) | unique_by({x, y, z}))
+  })
+' $TARGET.tmp $missingFile > $TARGET
+
+# Clean up the temporary files
+rm $TARGET.tmp
+rm $inputFile
+
+# Calculate md5sum of the new gear data
+MD5=$(md5sum $TARGET_DIR/$TARGET | cut -d' ' -f1)
+
+# Update urls.json with the new md5sum for dataStaticServices
+jq '. = [.[] | if (.id == "dataStaticServices") then (.md5 = "'$MD5'") else . end]' < ../Data-Storage/urls.json > ../Data-Storage/urls.json.tmp
+mv ../Data-Storage/urls.json.tmp ../Data-Storage/urls.json
