96d71bfaa499264616bd6a53c93289909686f1d6
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2024-08-10 17:19:04 +0200

    feat: Upgrade map files for Wynncraft 2.1 release (#140)
    This PR should be ready to merge when the release is happening.
    
    Below is the full map (without being broken into parts):
    
    ![map-main-full](https://github.com/user-attachments/assets/23d6b0e5-e350-4fee-bc2a-fbccf398514e)

--- Data-Storage/raw/map/update-journeymap.sh
+++ Data-Storage/raw/map/update-journeymap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script helps the user update the map for Wynntils, using journeymap.
-# Please install journeymap 5.9, found in the bin directory.
+# Please install journeymap 6.0, found in the bin directory.
 #
 # Made by magicus (https://github.com/magicus)
 #
@@ -24,33 +24,33 @@ fi
 if [[ $COMMAND = "get-from-journeymap" ]]; then
   mkdir -p $WYNNDATA_DIR/journeymap-data/DIM0/day
   echo "Changed files:"
-  diff -q journeymap/data/mp/$WYNNCRAFT_WORLD_NAME/DIM0/day $WYNNDATA_DIR/journeymap-data/DIM0/day
-  cp -f journeymap/data/mp/$WYNNCRAFT_WORLD_NAME/DIM0/day/* $WYNNDATA_DIR/journeymap-data/DIM0/day
+  diff -q journeymap/data/mp/$WYNNCRAFT_WORLD_NAME/DIM0/day $WYNNDATA_DIR/raw_map
+  cp -f journeymap/data/mp/$WYNNCRAFT_WORLD_NAME/DIM0/day/* $WYNNDATA_DIR/raw_map
   echo "Please go to $WYNNDATA_DIR and commit and push your changes"
 elif [[ $COMMAND == "install-in-journeymap" ]]; then
   mkdir -p journeymap/data/mp/$WYNNCRAFT_WORLD_NAME/DIM0/day
   echo "Changed files:"
-  diff -q $WYNNDATA_DIR/journeymap-data/DIM0/day journeymap/data/mp/$WYNNCRAFT_WORLD_NAME/DIM0/day
-  cp -f $WYNNDATA_DIR/journeymap-data/DIM0/day/* journeymap/data/mp/$WYNNCRAFT_WORLD_NAME/DIM0/day
+  diff -q $WYNNDATA_DIR/raw_map journeymap/data/mp/$WYNNCRAFT_WORLD_NAME/DIM0/day
+  cp -f $WYNNDATA_DIR/raw_map/* journeymap/data/mp/$WYNNCRAFT_WORLD_NAME/DIM0/day
   echo "Your journeymap installation now has the latest map data"
 elif [[ $COMMAND == "install-wynntils-config" ]]; then
-  if [[ ! -d journeymap/config/5.9 ]]; then
-      echo "Cannot find journeymap 5.9 config directory"
-      echo "Please verify that you are in the correct directory and that you have journeymap 5.9 installed"
+  if [[ ! -d journeymap/config/6.0 ]]; then
+      echo "Cannot find journeymap 6.0 config directory"
+      echo "Please verify that you are in the correct directory and that you have journeymap 6.0 installed"
       exit 1
   fi
-  cp -a journeymap/config/5.9/journeymap.core.config journeymap/config/5.9/journeymap.core.config.orig
+  cp -a journeymap/config/6.0/journeymap.core.config journeymap/config/6.0/journeymap.core.config.orig
   cp -a journeymap/colorpalette.json journeymap/colorpalette.json.orig
-  cp $WYNNDATA_DIR/config/journeymap.core.config journeymap/config/5.9/journeymap.core.config
+  cp $WYNNDATA_DIR/config/journeymap.core.config journeymap/config/6.0/journeymap.core.config
   cp $WYNNDATA_DIR/config/colorpalette.json journeymap/colorpalette.json
   echo "Replaced journeymap.core.config and colorpalette.json (backups saved as .orig)"
 elif [[ $COMMAND == "restore-orig-config" ]]; then
-  if [[ ! -d journeymap/config/5.9 ]]; then
-      echo "Cannot find journeymap 5.9 config directory"
-      echo "Please verify that you are in the correct directory and that you have journeymap 5.9 installed"
+  if [[ ! -d journeymap/config/6.0 ]]; then
+      echo "Cannot find journeymap 6.0 config directory"
+      echo "Please verify that you are in the correct directory and that you have journeymap 6.0 installed"
       exit 1
   fi
-  cp -a journeymap/config/5.9/journeymap.core.config.orig journeymap/config/5.9/journeymap.core.config
+  cp -a journeymap/config/6.0/journeymap.core.config.orig journeymap/config/6.0/journeymap.core.config
   cp -a journeymap/colorpalette.json.orig journeymap/colorpalette.json
   echo "Restored journeymap.core.config and colorpalette.json from backups"
 else
