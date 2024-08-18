d288c9d3486590b13a23fc2b3a61795ef1cd9cac
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2024-08-18 08:47:35 +0200

    chore: bump version number on md5sum change (#153)
    I believe this version bumping is not strictly necessary, but we might
    as well use it to indicate changes.

--- Generators/extract_caves.sh
+++ Generators/extract_caves.sh
@@ -13,4 +13,10 @@ MD5=$(md5sum $OUTPUT_DIR/caves.json | cut -d' ' -f1)
 
 # Update urls.json with the new md5sum for dataStaticCaveInfo
 jq '. = [.[] | if (.id == "dataStaticCaveInfo") then (.md5 = "'$MD5'") else . end]' < $BASE_DIR/Data-Storage/urls.json > $BASE_DIR/Data-Storage/urls.json.tmp
-mv $BASE_DIR/Data-Storage/urls.json.tmp $BASE_DIR/Data-Storage/urls.json
+
+# If the temp file is different from the original, bump the version number
+if ! cmp -s ../Data-Storage/urls.json ../Data-Storage/urls.json.tmp; then
+    jq 'map(if has("version") then .version += 1 else . end)' < $BASE_DIR/Data-Storage/urls.json.tmp > $BASE_DIR/Data-Storage/urls.json
+fi
+
+rm $BASE_DIR/Data-Storage/urls.json.tmp
